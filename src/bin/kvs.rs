b4648494e9a278c076c5563693f2f451a73696ff
psinghal20 <psinghal20@gmail.com>
2019-07-07 23:38:44 +0530

    refactor: replace clap with structopt
    Signed-off-by: psinghal20 <psinghal20@gmail.com>

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,42 +1,43 @@
-#[macro_use]
-extern crate clap;
+extern crate structopt;
 use kvs::KvStore;
 use std::process;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Opt {
+    #[structopt(short = "V", long = "version")]
+    version: bool,
+    #[structopt(subcommand)]
+    cmd: Option<Cmd>,
+}
+
+#[derive(StructOpt)]
+enum Cmd {
+    #[structopt(name = "get")]
+    Get { key: String },
+    #[structopt(name = "set")]
+    Set { key: String, value: String },
+    #[structopt(name = "rm")]
+    Remove { key: String },
+}
 
 fn main() {
-    let matches = clap_app!(kvs =>
-        (name: env!("CARGO_PKG_NAME"))
-        (version: env!("CARGO_PKG_VERSION"))
-        (author: env!("CARGO_PKG_AUTHORS"))
-        (about: env!("CARGO_PKG_DESCRIPTION"))
-        (@arg version: -V --version "returns the version of the kvs")
-        (@subcommand get =>
-            (about: "Gets the value for give key")
-            (@arg KEY: +required)
-        )
-        (@subcommand set =>
-            (about: "Sets the given value for the given key")
-            (@arg KEY: +required)
-            (@arg VALUE: +required)
-        )
-        (@subcommand rm =>
-            (about: "Removes the given key from the store")
-            (@arg KEY: +required)
-        )
-    )
-    .get_matches();
+    let opt = Opt::from_args();
     let mut kv_store = KvStore::new();
 
-    if let Some(matches) = matches.subcommand_matches("get") {
-        kv_store.get(matches.value_of("KEY").unwrap().to_string());
-    } else if let Some(matches) = matches.subcommand_matches("set") {
-        kv_store.set(
-            matches.value_of("KEY").unwrap().to_string(),
-            matches.value_of("VALUE").unwrap().to_string(),
-        );
-    } else if let Some(matches) = matches.subcommand_matches("rm") {
-        kv_store.remove(matches.value_of("KEY").unwrap().to_string());
-    } else if matches.is_present("version") {
+    if let Some(cmd) = opt.cmd {
+        match cmd {
+            Cmd::Get { key } => {
+                println!("{}", kv_store.get(key.to_string()).unwrap());
+            }
+            Cmd::Set { key, value } => {
+                kv_store.set(key.to_string(), value.to_string());
+            }
+            Cmd::Remove { key } => {
+                kv_store.remove(key.to_string());
+            }
+        }
+    } else if opt.version {
         println!(env!("CARGO_PKG_VERSION"));
     } else {
         process::exit(1);
