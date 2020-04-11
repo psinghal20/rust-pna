563da57100c17b5f02fba8428495b9acd3cb2f4a
psinghal20 <psinghal20@gmail.com>
2020-04-11 11:22:58 +0530

    feat: add get benchmarks for sled and kvs
    Signed-off-by: psinghal20 <psinghal20@gmail.com>

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,15 +2,15 @@ use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use kvs::{KvStore, KvsEngine, SledStore};
 use tempfile::TempDir;
 
-fn bench_kv_write(c: &mut Criterion) {
-    c.bench_function("KVS Write", |b| {
+fn bench_kv_set(c: &mut Criterion) {
+    c.bench_function("KVS set", |b| {
         b.iter_batched(
             || {
                 let temp_dir = TempDir::new().unwrap();
                 KvStore::open(&temp_dir.path()).unwrap()
             },
             |mut store| {
-                for i in 1..(1 << 3) {
+                for i in 1..(1 << 10) {
                     store.set(format!("key{}", i), "value".to_string()).unwrap();
                 }
             },
@@ -19,15 +19,39 @@ fn bench_kv_write(c: &mut Criterion) {
     });
 }
 
-fn bench_sled_write(c: &mut Criterion) {
-    c.bench_function("Sled Write", |b| {
+fn bench_kv_get(c: &mut Criterion) {
+    c.bench_function("KVS get", |b| {
+        b.iter_batched(
+            || {
+                let temp_dir = TempDir::new().unwrap();
+                let mut store = KvStore::open(&temp_dir.path()).unwrap();
+                for i in 1..(1 << 10) {
+                    store.set(format!("key{}", i), "value".to_string()).unwrap();
+                }
+                store
+            },
+            |mut store| {
+                for i in 1..(1 << 10) {
+                    assert_eq!(
+                        "value".to_string(),
+                        store.get(format!("key{}", i)).unwrap().unwrap()
+                    );
+                }
+            },
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+fn bench_sled_set(c: &mut Criterion) {
+    c.bench_function("Sled set", |b| {
         b.iter_batched(
             || {
                 let temp_dir = TempDir::new().unwrap();
                 SledStore::open(&temp_dir.path()).unwrap()
             },
             |mut store| {
-                for i in 1..(1 << 3) {
+                for i in 1..(1 << 10) {
                     store.set(format!("key{}", i), "value".to_string()).unwrap();
                 }
             },
@@ -36,5 +60,35 @@ fn bench_sled_write(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, bench_kv_write, bench_sled_write);
+fn bench_sled_get(c: &mut Criterion) {
+    c.bench_function("Sled get", |b| {
+        b.iter_batched(
+            || {
+                let temp_dir = TempDir::new().unwrap();
+                let mut store = SledStore::open(&temp_dir.path()).unwrap();
+                for i in 1..(1 << 10) {
+                    store.set(format!("key{}", i), "value".to_string()).unwrap();
+                }
+                store
+            },
+            |mut store| {
+                for i in 1..(1 << 10) {
+                    assert_eq!(
+                        "value".to_string(),
+                        store.get(format!("key{}", i)).unwrap().unwrap()
+                    );
+                }
+            },
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+criterion_group!(
+    benches,
+    bench_kv_set,
+    bench_kv_get,
+    bench_sled_set,
+    bench_sled_get
+);
 criterion_main!(benches);
